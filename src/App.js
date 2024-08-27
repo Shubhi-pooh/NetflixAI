603694f6411ceb644a7599224b95d7dab21b2848
Shubhangi-Verma <135640936+Shubhangi-Verma@users.noreply.github.com>
2024-08-28 01:00:48 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ function handleClick (name) {
+  alert(`Hi ${name}`)
+ }
+
   return (
     <div className="App">
       <h3 className='hello'>Hello Shubhi</h3>
       <div className='text-red-500 text-xl font-bold'>ANKUR SIR</div>
+      <button onClick={() => handleClick("Shubhi")} className='border border-gray-300 bg-blue-300 text-white font-extrabold text-2xl px-10 py-2 rounded-xl '>ADD</button>
     </div>
   );
 }
