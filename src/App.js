cd51b2399049cdfc6f9c5599c5cefaf579b51a44
Shubhangi-Verma <135640936+Shubhangi-Verma@users.noreply.github.com>
2024-08-28 00:26:07 +0530

    Fix tailwind config

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h3 className='hello'>Hello Shubhi</h3>
-      <div className='text-xl'>JASS</div>
+      <div className='text-red-500 text-xl font-bold'>ANKUR SIR</div>
     </div>
   );
 }
